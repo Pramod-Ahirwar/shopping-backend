26887f6a1d73b62977f318cf8f3b26c685d01d0b
Pramod Ahirwar <pramodahirwar965@gmail.com>
2025-12-17 19:17:50 +0530

    Backend ready

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+// routes
+const productRoutes = require("./routes/productRoutes");
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// products API
+app.use("/api/products", productRoutes);
+
+// test route
+app.get("/", (req, res) => {
+  res.send("Shopping API Running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
